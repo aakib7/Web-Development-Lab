17c7d5c0c830dd408328486ab138bb038a8e57d4
aakib7 <ajmehdi5@gmail.com>
2021-11-09 13:42:44 +0500

    new files

---
+++ JQuery/multipleEventBinding/main.js
@@ -0,0 +1,38 @@
+$('document').ready(function() {
+    $('.clickBtn').on('click mouseover mouseout', eventAdd);
+    $('#disableButton').on('click',disableHandle);
+    $('#enableButton').on('click',function(event){
+    $('.clickBtn').on('mouseover',eventAdd);
+
+    });
+    
+    // function(event){ // event binding
+    //     // if(event.type=='click'){
+    //     //     console.log('click');
+    //     // }
+    //     // else if(event.type=='mouseover'){
+    //     //     $('.clickBtn').addClass('red-class');
+    //     // }
+    //     // else{
+    //     //      $('.clickBtn').removeClass('red-class');
+    //     // }
+    // }
+    // )
+});
+
+function eventAdd(event){
+    if(event.type=='click'){
+            console.log('click');
+        }
+        else if(event.type=='mouseover'){
+            $('.clickBtn').addClass('red-class');
+        }
+        else{
+             $('.clickBtn').removeClass('red-class');
+        }
+}
+
+function disableHandle(){
+    console.log('click disable');
+    $('.clickBtn').off('mouseover');}
+
